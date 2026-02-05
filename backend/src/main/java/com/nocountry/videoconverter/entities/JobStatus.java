339f8e57c3ca27849534aab6483d46c0805da6ae
Iván Sierra <sierraivan93@gmail.com>
2026-02-04 22:26:26 -0300

    feat: jobstatus enum with description

--- backend/src/main/java/com/nocountry/videoconverter/entities/JobStatus.java
+++ backend/src/main/java/com/nocountry/videoconverter/entities/JobStatus.java
@@ -1,11 +1,24 @@
 package com.nocountry.videoconverter.entities;
 
-
+/**
+ * Descripción del estado de la conversión para optimizar la visualización.
+ */
 public enum JobStatus {
 
-    PENDING,
-    PROCESSING,
-    COMPLETED,
-    FAILED,
-    EXPIRED;
+    PENDING("PENDING"),
+    PROCESSING("PROCESSING"),
+    COMPLETED("COMPLETED"),
+    FAILED("FAILED"),
+    EXPIRED("EXPIRED");
+
+    private String description;
+
+    JobStatus(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
